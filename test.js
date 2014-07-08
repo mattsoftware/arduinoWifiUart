f2393425b97b8e7a7479cd66101bb3fb612fe2f9
Matt Paine <matt@mattsoftware.com>
2014-07-08 10:11:07 +1000

    Cleaned up some whitespace

--- test.js
+++ test.js
@@ -43,226 +43,226 @@ var usage = function () {
     process.exit(2);
 }
 if (!testFileSpec) {
-	usage();
+    usage();
 }
 
 var collectInoFilesToTest = function (baseDir) {
-	var inos = [];
-	baseDir = fs.realpathSync(baseDir);
+    var inos = [];
+    baseDir = fs.realpathSync(baseDir);
 
-	var baseDirStat = fs.statSync(baseDir);
-	if (baseDirStat.isFile()) {
-		inos.push(baseDir);
-	} else if (baseDirStat.isDirectory()) {
+    var baseDirStat = fs.statSync(baseDir);
+    if (baseDirStat.isFile()) {
+        inos.push(baseDir);
+    } else if (baseDirStat.isDirectory()) {
         var baseDirName = baseDir.substring(baseDir.lastIndexOf('/')+1);
         if (fs.existsSync(baseDir + '/' + baseDirName + '.ino')) {
             inos.push(baseDir + '/' + baseDirName + '.ino');
         } else {
-			var dirs = fs.readdirSync(baseDir);
-			for (var x in dirs) {
-				if (dirs.hasOwnProperty(x)) {
-					if (dirs[x] !== '.' && dirs[x] !== '..') {
-						inos.push(baseDir + '/' + dirs[x] + '/' + dirs[x] + '.ino');
-					}
-				}
-			}
-		}
-	}
+            var dirs = fs.readdirSync(baseDir);
+            for (var x in dirs) {
+                if (dirs.hasOwnProperty(x)) {
+                    if (dirs[x] !== '.' && dirs[x] !== '..') {
+                        inos.push(baseDir + '/' + dirs[x] + '/' + dirs[x] + '.ino');
+                    }
+                }
+            }
+        }
+    }
 
-	return inos;
+    return inos;
 };
 var inos = collectInoFilesToTest(testFileSpec);
 
 function runTests (dev, baud, callback) {
-	var serial = new SerialPort(dev, {
-		baudrate: baud,
-		parser: serialport.parsers.readline("\r\n")
-	});
-	serial.on('open', function () {
-		var testRunLines = [];
+    var serial = new SerialPort(dev, {
+        baudrate: baud,
+        parser: serialport.parsers.readline("\r\n")
+    });
+    serial.on('open', function () {
+        var testRunLines = [];
 
-		serial.on('data', function (line) {
-			if (/^Test .* failed\.$/.test(line)) {
-				process.stdout.write('F');
-			} else if (/^Test .* skipped\.$/.test(line)) {
-				process.stdout.write('S');
-			} else {
-				process.stdout.write('.');
-			}
-			if (/^Test .*\.$/.test(line) || /^Assertion failed:/.test(line)) {
-				testRunLines.push(line);
-			}
-			var parseSummary = /^Test summary: ([0-9]+) passed, ([0-9]+) failed, and ([0-9]+) skipped, out of ([0-9]+) test\(s\).$/;
-			var summaryResult = parseSummary.exec(line);
-			if (summaryResult) {
-				serial.flush(function (err) {
-					serial.drain(function (err) {
-						serial.close(function (err) {
-							process.stdout.write('\n');
-							callback(err, {
-								output: testRunLines,
-								passed: summaryResult[1],
-								failed: summaryResult[2],
-								skipped: summaryResult[3],
-								total: summaryResult[4]
-							});
-						});
-					});
-				});
-			}
-		});
-	});
-	serial.on('error', function (err) {
-		callback(err, null);
-	});
-	serial.on('exit', function () {
-		callback(null, 'exited');
-	});
+        serial.on('data', function (line) {
+            if (/^Test .* failed\.$/.test(line)) {
+                process.stdout.write('F');
+            } else if (/^Test .* skipped\.$/.test(line)) {
+                process.stdout.write('S');
+            } else {
+                process.stdout.write('.');
+            }
+            if (/^Test .*\.$/.test(line) || /^Assertion failed:/.test(line)) {
+                testRunLines.push(line);
+            }
+            var parseSummary = /^Test summary: ([0-9]+) passed, ([0-9]+) failed, and ([0-9]+) skipped, out of ([0-9]+) test\(s\).$/;
+            var summaryResult = parseSummary.exec(line);
+            if (summaryResult) {
+                serial.flush(function (err) {
+                    serial.drain(function (err) {
+                        serial.close(function (err) {
+                            process.stdout.write('\n');
+                            callback(err, {
+                                output: testRunLines,
+                                passed: summaryResult[1],
+                                failed: summaryResult[2],
+                                skipped: summaryResult[3],
+                                total: summaryResult[4]
+                            });
+                        });
+                    });
+                });
+            }
+        });
+    });
+    serial.on('error', function (err) {
+        callback(err, null);
+    });
+    serial.on('exit', function () {
+        callback(null, 'exited');
+    });
 };
 
 var arduinoCommand = function (arduinoOptions, callback) {
-	var output = '';
-	var errOutput = '';
-	var boardParam = [arduinoOptions.package, arduinoOptions.architecture, arduinoOptions.board].join(':');
-	if (arduinoOptions.parameters.length > 0) {
-		boardParam = [boardParam, arduinoOptions.parameters].join(':');
-	}
-	var cmdParams = [
-		'--' + arduinoOptions.command,
-		'--board', boardParam,
-		'--port', arduinoOptions.port,
-		arduinoOptions.ino
-	];
+    var output = '';
+    var errOutput = '';
+    var boardParam = [arduinoOptions.package, arduinoOptions.architecture, arduinoOptions.board].join(':');
+    if (arduinoOptions.parameters.length > 0) {
+        boardParam = [boardParam, arduinoOptions.parameters].join(':');
+    }
+    var cmdParams = [
+        '--' + arduinoOptions.command,
+        '--board', boardParam,
+        '--port', arduinoOptions.port,
+        arduinoOptions.ino
+    ];
     if (debug) {
         console.log("Running arduino command: " + arduinoOptions.binary + " " + cmdParams.join(" "));
     }
-	var cmd = childProcess.spawn(arduinoOptions.binary, cmdParams);
-	cmd.on('error', function (err) {
+    var cmd = childProcess.spawn(arduinoOptions.binary, cmdParams);
+    cmd.on('error', function (err) {
     if (debug) {
         console.log("Arduino command failed");
     }
-		callback(err, null);
-	});
-	cmd.stdout.on('data', function(data) {
-		output += data;
-	});
-	cmd.stderr.on('data', function(data) {
-		errOutput += data;
-	});
-	cmd.on('close', function(code){
-		if (code !== 0) {
-			var error = new Error(arduinoOptions.command + ' exited with code ' + code);
-			error.code = code;
-			error.commandLine = arduinoOptions.binary + ' ' + cmdParams.join(' ');
-			error.output = output;
-			error.errorOutput = errOutput;
-			callback(error, null);
-		} else {
-			callback(null, output);
-		}
-	});
+        callback(err, null);
+    });
+    cmd.stdout.on('data', function(data) {
+        output += data;
+    });
+    cmd.stderr.on('data', function(data) {
+        errOutput += data;
+    });
+    cmd.on('close', function(code){
+        if (code !== 0) {
+            var error = new Error(arduinoOptions.command + ' exited with code ' + code);
+            error.code = code;
+            error.commandLine = arduinoOptions.binary + ' ' + cmdParams.join(' ');
+            error.output = output;
+            error.errorOutput = errOutput;
+            callback(error, null);
+        } else {
+            callback(null, output);
+        }
+    });
 };
 
 var arduinoVerify = function (ino, arduinoOptions, callback) {
-	var verifyArduinoOptions = JSON.parse(JSON.stringify(arduinoOptions));
-	verifyArduinoOptions.command = 'verify';
-	verifyArduinoOptions.ino = ino;
-	arduinoCommand(verifyArduinoOptions, callback);
+    var verifyArduinoOptions = JSON.parse(JSON.stringify(arduinoOptions));
+    verifyArduinoOptions.command = 'verify';
+    verifyArduinoOptions.ino = ino;
+    arduinoCommand(verifyArduinoOptions, callback);
 };
 
 var arduinoUpload = function (ino, arduinoOptions, callback) {
-	var uploadArduinoOptions = JSON.parse(JSON.stringify(arduinoOptions));
-	uploadArduinoOptions.command = 'upload';
-	uploadArduinoOptions.ino = ino;
-	arduinoCommand(uploadArduinoOptions, callback);
+    var uploadArduinoOptions = JSON.parse(JSON.stringify(arduinoOptions));
+    uploadArduinoOptions.command = 'upload';
+    uploadArduinoOptions.ino = ino;
+    arduinoCommand(uploadArduinoOptions, callback);
 }
 
 var arduinoOptions = {
-	binary: '/Applications/Arduino.app/Contents/MacOS/JavaApplicationStub',
-	package: 'arduino',
-	architecture: 'avr',
-    board: board,
-	parameters: '',
-	port: dev
+    binary: '/Applications/Arduino.app/Contents/MacOS/JavaApplicationStub',
+    package: 'arduino',
+    architecture: 'avr',
+board: board,
+    parameters: '',
+    port: dev
 };
 
 var fullTestRun = function (ino, arduinoOptions, baud, previousResults) {
-	if (!previousResults) {
-		previousResults = [];
-	}
-	console.log("Running tests for " + ino);
-	var ret = Q.defer();
-	//Q.fcall(function() { return Q.nfcall(arduinoVerify, ino, arduinoOptions); })
-	//.then(function () { return Q.nfcall(arduinoUpload, ino, arduinoOptions); })
-	Q.fcall(function() { return Q.nfcall(arduinoUpload, ino, arduinoOptions); })
-	.then(function () { return Q.nfcall(runTests, arduinoOptions.port, baud); })
-	.then(function (result) {
-		result.filename = ino;
-		previousResults.push(result);
-		ret.resolve(previousResults);
-	})
-	.fail(function (err) {
-		ret.reject(err);
-	});
-	return ret.promise;
+    if (!previousResults) {
+        previousResults = [];
+    }
+    console.log("Running tests for " + ino);
+    var ret = Q.defer();
+    //Q.fcall(function() { return Q.nfcall(arduinoVerify, ino, arduinoOptions); })
+    //.then(function () { return Q.nfcall(arduinoUpload, ino, arduinoOptions); })
+    Q.fcall(function() { return Q.nfcall(arduinoUpload, ino, arduinoOptions); })
+    .then(function () { return Q.nfcall(runTests, arduinoOptions.port, baud); })
+    .then(function (result) {
+        result.filename = ino;
+        previousResults.push(result);
+        ret.resolve(previousResults);
+    })
+    .fail(function (err) {
+        ret.reject(err);
+    });
+    return ret.promise;
 };
 var fullTestRunPromise = function (ino, arduinoOptions, baud) {
-	return function (result) {
-		return fullTestRun(ino, arduinoOptions, baud, result);
-	};
+    return function (result) {
+        return fullTestRun(ino, arduinoOptions, baud, result);
+    };
 }
 
 var promise = Q(null);
 for (var ino in inos) {
-	if (inos.hasOwnProperty(ino)) {
-		promise = promise.then(fullTestRunPromise(inos[ino], arduinoOptions, baud));
-	}
+    if (inos.hasOwnProperty(ino)) {
+        promise = promise.then(fullTestRunPromise(inos[ino], arduinoOptions, baud));
+    }
 }
 
 var summariseResult = function (result) {
-	//console.log(result);
-	var summary = {
-		passed: 0,
-		failed: 0,
-		skipped: 0,
-		total: 0,
-		failedTests: [],
-	};
-	for (var r in result) {
-		if (result.hasOwnProperty(r)) {
-			summary.passed += +result[r].passed;
-			summary.failed += +result[r].failed;
-			summary.skipped += +result[r].skipped;
-			summary.total += +result[r].total;
-			if (+result[r].failed > 0) {
-				for (var l in result[r].output) {
-					if (result[r].output.hasOwnProperty(l)) {
-						var line = result[r].output[l];
-						if (/^Assertion failed:/.test(line)) {
-							summary.failedTests.push({
-								filename: result[r].filename,
-								line: line
-							});
-						}
-					}
-				}
-			}
-		}
-	}
-	return summary;
+    //console.log(result);
+    var summary = {
+        passed: 0,
+        failed: 0,
+        skipped: 0,
+        total: 0,
+        failedTests: [],
+    };
+    for (var r in result) {
+        if (result.hasOwnProperty(r)) {
+            summary.passed += +result[r].passed;
+            summary.failed += +result[r].failed;
+            summary.skipped += +result[r].skipped;
+            summary.total += +result[r].total;
+            if (+result[r].failed > 0) {
+                for (var l in result[r].output) {
+                    if (result[r].output.hasOwnProperty(l)) {
+                        var line = result[r].output[l];
+                        if (/^Assertion failed:/.test(line)) {
+                            summary.failedTests.push({
+                                filename: result[r].filename,
+                                line: line
+                            });
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return summary;
 };
 
 promise.then(function (result) {
-	//console.log(result);
-	var summarised = summariseResult(result);
-	console.log(summarised);
+    //console.log(result);
+    var summarised = summariseResult(result);
+    console.log(summarised);
 })
 .fail(function(err) {
-	console.log("ERROR: " + err + " (" + err.code + ")");
-	if (err.commandLine) {
-		console.log(err.commandLine.trim());
-		console.log("-----");
-		console.log(err.errorOutput.trim());
-		console.log("-----");
-	}
+    console.log("ERROR: " + err + " (" + err.code + ")");
+    if (err.commandLine) {
+        console.log(err.commandLine.trim());
+        console.log("-----");
+        console.log(err.errorOutput.trim());
+        console.log("-----");
+    }
 });

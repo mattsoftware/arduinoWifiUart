14263b911977e483c81ee6959ddbe58d473f4782
Matt Paine <matt@mattsoftware.com>
2014-07-12 08:11:03 +1000

    fix some whitespace before working on the file

--- examples/SerialControl/SerialControl.ino
+++ examples/SerialControl/SerialControl.ino
@@ -71,21 +71,21 @@ bool writeToWifi = false;
 
 
 void userOne() {
-  // set module to connect to home wifi
-  if (!WifiUartNetwork::setWirelessMode(&wifi, WIFIU_NET_MODE_INFRASTRUCTURE)) {
-    Serial.println("Could not set wireless mode");
-  }
-  if (!WifiUartNetwork::setSSID(&wifi, SSID)) {
-    Serial.println("Could not set SSID");
-  }
-  if (!WifiUartNetwork::setSecurity(&wifi, WIFIU_NET_ENC_WPA, SECURITY_PASSWORD)) {
-    Serial.println("Could not set security");
-  }
-  if (!WifiUartNetwork::setNetworkSettingsToDHCP(&wifi)) {
-    Serial.println("Could not set dhcp");
-  }
-  Serial.print("Done (Don't forget to reset the module ");
-  Serial.println("for the changes to take effect)");
+    // set module to connect to home wifi
+    if (!WifiUartNetwork::setWirelessMode(&wifi, WIFIU_NET_MODE_INFRASTRUCTURE)) {
+        Serial.println("Could not set wireless mode");
+    }
+    if (!WifiUartNetwork::setSSID(&wifi, SSID)) {
+        Serial.println("Could not set SSID");
+    }
+    if (!WifiUartNetwork::setSecurity(&wifi, WIFIU_NET_ENC_WPA, SECURITY_PASSWORD)) {
+        Serial.println("Could not set security");
+    }
+    if (!WifiUartNetwork::setNetworkSettingsToDHCP(&wifi)) {
+        Serial.println("Could not set dhcp");
+    }
+    Serial.print("Done (Don't forget to reset the module ");
+    Serial.println("for the changes to take effect)");
 }
 
 void userTwo() {
@@ -112,13 +112,13 @@ void userThree() {
 
 
 bool reportSuccess(bool success) {
-  if (success) {
-    Serial.println("Success");
-  }
-  else {
-    Serial.println("Failed");
-  }
-  return success;
+    if (success) {
+        Serial.println("Success");
+    }
+    else {
+        Serial.println("Failed");
+    }
+    return success;
 }
 
 void updateCommandLed () {
@@ -131,106 +131,106 @@ void updateCommandLed () {
 }
 
 void helpLine(char shortCut, char* description) {
-  Serial.print("    ");
-  Serial.print(shortCut);
-  Serial.print(": ");
-  Serial.println(description);
+    Serial.print("    ");
+    Serial.print(shortCut);
+    Serial.print(": ");
+    Serial.println(description);
 }
 
 void help()
 {
-  Serial.println("Commands available:");
-  helpLine(TOGGLE_INPUT, "Toggle input to wifimodule");
-  helpLine(HELP, "This help message");
-  helpLine(ENTER_COMMAND, "Enter command mode");
-  helpLine(EXIT_COMMAND, "Exit command mode");
-  helpLine(FACTORY_RESET, "Factory Reset");
-  helpLine(RESET, "Reset");
-  helpLine(HARDWARE_RESET, "Hardware Reset");
-  helpLine(NETWORK_STATE, "Network State");
-  helpLine(NETWORK_SETTINGS, "Network Settings");
-  Serial.print("Wifi module is currently ");
-  Serial.print(wifi.isInCommandMode() ? "" : "not ");
-  Serial.println("in command mode");
-  updateCommandLed();
+    Serial.println("Commands available:");
+    helpLine(TOGGLE_INPUT, "Toggle input to wifimodule");
+    helpLine(HELP, "This help message");
+    helpLine(ENTER_COMMAND, "Enter command mode");
+    helpLine(EXIT_COMMAND, "Exit command mode");
+    helpLine(FACTORY_RESET, "Factory Reset");
+    helpLine(RESET, "Reset");
+    helpLine(HARDWARE_RESET, "Hardware Reset");
+    helpLine(NETWORK_STATE, "Network State");
+    helpLine(NETWORK_SETTINGS, "Network Settings");
+    Serial.print("Wifi module is currently ");
+    Serial.print(wifi.isInCommandMode() ? "" : "not ");
+    Serial.println("in command mode");
+    updateCommandLed();
 }
 
 void networkState() {
-  Serial.println("-----");
-  WifiUartNetModeResponse mode = WifiUartNetwork::getWirelessMode(&wifi);
-  if (mode.code == WIFIU_NET_RESPONSE_OK && mode.mode == WIFIU_NET_MODE_ADHOC) {
-    Serial.println("AdHoc Mode");
-  }
-  else if (mode.code == WIFIU_NET_RESPONSE_OK && mode.mode == WIFIU_NET_MODE_INFRASTRUCTURE) {
-    Serial.println("Infrastructure Mode");
-  }
-  else {
-    Serial.println("Error getting mode of wireless");
-  }
+    Serial.println("-----");
+    WifiUartNetModeResponse mode = WifiUartNetwork::getWirelessMode(&wifi);
+    if (mode.code == WIFIU_NET_RESPONSE_OK && mode.mode == WIFIU_NET_MODE_ADHOC) {
+        Serial.println("AdHoc Mode");
+    }
+    else if (mode.code == WIFIU_NET_RESPONSE_OK && mode.mode == WIFIU_NET_MODE_INFRASTRUCTURE) {
+        Serial.println("Infrastructure Mode");
+    }
+    else {
+        Serial.println("Error getting mode of wireless");
+    }
 
-  WifiUartNetSSIDResponse ssid = WifiUartNetwork::getSSID(&wifi);
-  if (ssid.code == WIFIU_NET_RESPONSE_OK) {
-    Serial.print("SSID: "); Serial.println(ssid.ssid);
-  }
-  else {
-    Serial.println("Error getting ssid");
-  }
+    WifiUartNetSSIDResponse ssid = WifiUartNetwork::getSSID(&wifi);
+    if (ssid.code == WIFIU_NET_RESPONSE_OK) {
+        Serial.print("SSID: "); Serial.println(ssid.ssid);
+    }
+    else {
+        Serial.println("Error getting ssid");
+    }
 
-  WifiUartNetStateResponse state = WifiUartNetwork::getConnectionState(&wifi);
-  if (state.code == WIFIU_NET_RESPONSE_OK && state.isConnected) {
-    Serial.println("Connected!");
-    Serial.print("IP: "); Serial.println(state.ip);
-    Serial.print("Network Mask: "); Serial.println(state.netmask);
-    Serial.print("Gateway: "); Serial.println(state.gateway);
-    Serial.print("DNS: "); Serial.println(state.dns);
-    Serial.print("Mac: "); Serial.println(state.physical);
-  }
-  else if (state.code == WIFIU_NET_RESPONSE_OK) {
-    Serial.println("Not Connected");
-  }
-  else {
-    Serial.println("Error getting connection status");
-  }
-  Serial.println("-----");
+    WifiUartNetStateResponse state = WifiUartNetwork::getConnectionState(&wifi);
+    if (state.code == WIFIU_NET_RESPONSE_OK && state.isConnected) {
+        Serial.println("Connected!");
+        Serial.print("IP: "); Serial.println(state.ip);
+        Serial.print("Network Mask: "); Serial.println(state.netmask);
+        Serial.print("Gateway: "); Serial.println(state.gateway);
+        Serial.print("DNS: "); Serial.println(state.dns);
+        Serial.print("Mac: "); Serial.println(state.physical);
+    }
+    else if (state.code == WIFIU_NET_RESPONSE_OK) {
+        Serial.println("Not Connected");
+    }
+    else {
+        Serial.println("Error getting connection status");
+    }
+    Serial.println("-----");
 }
 
 void networkSettings() {
-  Serial.println("-----");
-  WifiUartNetSettingsResponse state = WifiUartNetwork::getNetworkSettings(&wifi);
-  if (state.code == WIFIU_NET_RESPONSE_OK && state.staticIp) {
-    Serial.println("Static Ip Settings:");
-    Serial.print("IP: "); Serial.println(state.ip);
-    Serial.print("Network Mask: "); Serial.println(state.netmask);
-    Serial.print("Gateway: "); Serial.println(state.gateway);
-    Serial.print("DNS: "); Serial.println(state.dns);
-  }
-  else if (state.code == WIFIU_NET_RESPONSE_OK) {
-    Serial.println("DHCP");
-  }
-  else {
-    Serial.println("Error getting settings");
-  }
-  Serial.println("-----");
+    Serial.println("-----");
+    WifiUartNetSettingsResponse state = WifiUartNetwork::getNetworkSettings(&wifi);
+    if (state.code == WIFIU_NET_RESPONSE_OK && state.staticIp) {
+        Serial.println("Static Ip Settings:");
+        Serial.print("IP: "); Serial.println(state.ip);
+        Serial.print("Network Mask: "); Serial.println(state.netmask);
+        Serial.print("Gateway: "); Serial.println(state.gateway);
+        Serial.print("DNS: "); Serial.println(state.dns);
+    }
+    else if (state.code == WIFIU_NET_RESPONSE_OK) {
+        Serial.println("DHCP");
+    }
+    else {
+        Serial.println("Error getting settings");
+    }
+    Serial.println("-----");
 }
 
 bool currentWebState () {
-  bool ret = true; // always assume the worst!
-  WifiUartMiscWebServiceResponse web = WifiUartMisc::getWebServiceStatus(&wifi);
-  if (web.code == WIFIU_MISC_RESPONSE_OK) {
-    ret = web.isEnabled;
-  }
-  return ret;
+    bool ret = true; // always assume the worst!
+    WifiUartMiscWebServiceResponse web = WifiUartMisc::getWebServiceStatus(&wifi);
+    if (web.code == WIFIU_MISC_RESPONSE_OK) {
+        ret = web.isEnabled;
+    }
+    return ret;
 }
 void webState() {
-  Serial.print("Webservice state: ");
-  Serial.println(currentWebState() ? "Enabled" : "Disabled");
+    Serial.print("Webservice state: ");
+    Serial.println(currentWebState() ? "Enabled" : "Disabled");
 }
 
 void webToggle() {
-  bool newState = !currentWebState();
-  Serial.print("Setting state to: ");
-  Serial.println(newState ? "Enabled" : "Disabled");
-  reportSuccess(WifiUartMisc::setWebServiceStatus(&wifi, newState, 80));
+    bool newState = !currentWebState();
+    Serial.print("Setting state to: ");
+    Serial.println(newState ? "Enabled" : "Disabled");
+    reportSuccess(WifiUartMisc::setWebServiceStatus(&wifi, newState, 80));
 }
 
 bool isTestLedOn = false;
@@ -264,87 +264,88 @@ void receiveChar(char c) {
 }
 
 void setup() {
-  Serial.begin(115200);
-  wifiSerial.begin(9600);
-  help();
-  pinMode(COMMAND_LED, OUTPUT);
-  pinMode(TEST_LED, OUTPUT);
-  wifi.setHardwareResetPin(RESET_PIN, true);
+    Serial.begin(115200);
+    wifiSerial.begin(9600);
+    help();
+    pinMode(COMMAND_LED, OUTPUT);
+    pinMode(TEST_LED, OUTPUT);
+    wifi.setHardwareResetPin(RESET_PIN, true);
 }
 
 void loop() {
-  if (wifiSerial.available()) {
-    while (wifiSerial.available()) {
-      receiveChar(wifiSerial.read());
+    if (wifiSerial.available()) {
+        while (wifiSerial.available()) {
+            receiveChar(wifiSerial.read());
+        }
     }
-  }
-  if (Serial.available()) {
-    char r = Serial.read();
-    if (r == TOGGLE_INPUT) {
-      writeToWifi = !writeToWifi;
-      Serial.print("Write to wifi is now ");
-      Serial.println(writeToWifi ? "on" : "off");
+    if (Serial.available()) {
+        char r = Serial.read();
+        if (r == TOGGLE_INPUT) {
+            writeToWifi = !writeToWifi;
+            Serial.print("Write to wifi is now ");
+            Serial.println(writeToWifi ? "on" : "off");
+        }
+        else if (writeToWifi) {
+            wifiSerial.print(r);
+            if (r == '\r') {
+                wifiSerial.print('\n');
+            }
+        }
+        else {
+            switch (r) {
+                case HELP:
+                    help();
+                    break;
+                case ENTER_COMMAND:
+                    Serial.println("Enter Command Mode");
+                    reportSuccess (wifi.enterCommandMode());
+                    updateCommandLed();
+                    break;
+                case EXIT_COMMAND:
+                    Serial.println("Exit Command Mode");
+                    reportSuccess (wifi.exitCommandMode());
+                    updateCommandLed();
+                    break;
+                case FACTORY_RESET:
+                    Serial.println("Factory Reset");
+                    reportSuccess (wifi.factoryReset());
+                    updateCommandLed();
+                    break;
+                case RESET:
+                    Serial.println("Reset");
+                    reportSuccess (wifi.reset());
+                    updateCommandLed();
+                    break;
+                case HARDWARE_RESET:
+                    Serial.println("Hardware Reset");
+                    reportSuccess (wifi.hardwareReset());
+                    updateCommandLed();
+                    break;
+                case NETWORK_STATE:
+                    Serial.println("Network State");
+                    networkState();
+                    break;
+                case NETWORK_SETTINGS:
+                    Serial.println("Network Settings");
+                    networkSettings();
+                    break;
+                case WEB_STATE:
+                    webState();
+                    break;
+                case WEB_TOGGLE:
+                    webToggle();
+                    break;
+                case USER1:
+                    userOne();
+                    break;
+                case USER2:
+                    userTwo();
+                    break;
+                case USER3:
+                    userThree();
+                    break;
+            }
+        }
     }
-    else if (writeToWifi) {
-      wifiSerial.print(r);
-      if (r == '\r') {
-          wifiSerial.print('\n');
-      }
-    }
-    else {
-      switch (r) {
-        case HELP:
-          help();
-          break;
-        case ENTER_COMMAND:
-          Serial.println("Enter Command Mode");
-          reportSuccess (wifi.enterCommandMode());
-          updateCommandLed();
-          break;
-        case EXIT_COMMAND:
-          Serial.println("Exit Command Mode");
-          reportSuccess (wifi.exitCommandMode());
-          updateCommandLed();
-          break;
-        case FACTORY_RESET:
-          Serial.println("Factory Reset");
-          reportSuccess (wifi.factoryReset());
-          updateCommandLed();
-          break;
-        case RESET:
-          Serial.println("Reset");
-          reportSuccess (wifi.reset());
-          updateCommandLed();
-          break;
-        case HARDWARE_RESET:
-          Serial.println("Hardware Reset");
-          reportSuccess (wifi.hardwareReset());
-          updateCommandLed();
-          break;
-        case NETWORK_STATE:
-          Serial.println("Network State");
-          networkState();
-          break;
-        case NETWORK_SETTINGS:
-          Serial.println("Network Settings");
-          networkSettings();
-          break;
-        case WEB_STATE:
-          webState();
-          break;
-        case WEB_TOGGLE:
-          webToggle();
-          break;
-        case USER1:
-          userOne();
-          break;
-        case USER2:
-          userTwo();
-          break;
-        case USER3:
-          userThree();
-          break;
-      }
-    }
-  }
 }
+
